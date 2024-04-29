cfaf440726867fa0c383c6ad865dfcd67f8c9779
Veysal Hasanbayov <hasanbayliveysaldev@gmail.com>
2024-04-29 16:10:26 +0400

    Initial Commit

---
+++ UsersList(SwiftUI)/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  UsersList(SwiftUI)
+//
+//  Created by Veysal Hasanbayli on 29.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UsersList(SwiftUI)/UsersList_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  UsersList_SwiftUI_App.swift
+//  UsersList(SwiftUI)
+//
+//  Created by Veysal Hasanbayli on 29.04.24.
+//
+
+import SwiftUI
+
+@main
+struct UsersList_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
