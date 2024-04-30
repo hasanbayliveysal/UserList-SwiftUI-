0c2234388acabc2acc427b2da6649d13f68bebae
Veysal Hasanbayov <113264766+hasanbayliveysal@users.noreply.github.com>
2024-04-30 14:14:31 +0400

    Update AlertView.swift

--- UsersList(SwiftUI)/Utils/AlertView.swift
+++ UsersList(SwiftUI)/Utils/AlertView.swift
@@ -5,18 +5,3 @@
 //  Created by Veysal Hasanbayli on 29.04.24.
 //
 
-import SwiftUI
-
-struct AlertView: View {
-    var body: some View {
-        
-        .alert(ispr)
-       
-    }
-}
-
-struct AlertView_Previews: PreviewProvider {
-    static var previews: some View {
-        AlertView()
-    }
-}
